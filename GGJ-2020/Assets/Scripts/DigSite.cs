07ffa6daf76d02dd6e5ddb2c5f2cc949bc1c90fc
William <liams_1998@hotmail.com>
2021-02-06 06:22:32 +0200

    Add dig site modal

---
+++ GGJ-2020/Assets/Scripts/DigSite.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DigSite : MonoBehaviour
+{
+    private int countTillDigDone = 10;
+    public void DoDig(int amount)
+    {
+        countTillDigDone -= amount;
+    }
+}
