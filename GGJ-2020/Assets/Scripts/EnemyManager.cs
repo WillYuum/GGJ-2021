d754a2ecadf49429a26eafe4642e9bd696e6c429
William <liams_1998@hotmail.com>
2021-02-20 03:07:46 +0200

    Create enemy manager script + add to scene

---
+++ GGJ-2020/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public static EnemyManager instance = null;
+
+
+    private List<Actor> allEnemies = new List<Actor>();
+
+    public GameObject[] spawnPoints;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            StartSpawningEnemies();
+        }
+    }
+
+    public void StartSpawningEnemies()
+    {
+        GameObject enemy = SpawnManager.instance.GetEnemy();
+        int randIndex = Random.Range(0, spawnPoints.Length - 1);
+        print("Check here: " + randIndex);
+        Transform selectPositionToSpawn = spawnPoints[randIndex].transform;
+        enemy.transform.position = selectPositionToSpawn.position;
+    }
+
+}
